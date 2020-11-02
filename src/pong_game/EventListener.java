59c0d90e3441b809a2eb6276a334b31e7cf0c342
ArtNascto <diamond.elite014@gmail.com>
2020-11-01 22:00:26 -0200

    feat: Initial Commit

---
+++ src/pong_game/EventListener.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong_game;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+/**
+ *
+ * @author seishuku
+ */
+public class EventListener implements GLEventListener {
+
+    public void init(GLAutoDrawable drawable) {
+        System.out.println("oi");
+    }
+
+    public void dispose(GLAutoDrawable drawable) {
+    }
+
+    public void display(GLAutoDrawable drawable) {
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    }
+
+}
---
+++ src/pong_game/Renderer.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong_game;
+
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.util.FPSAnimator;
+
+/**
+ *
+ * @author seishuku
+ */
+public class Renderer {
+
+    private static GLWindow window = null;
+    public static int screenWidth = 600;  //1280  - 640
+    public static int screenHeight = 600; //960  - 480
+
+    //Cria a janela de rendeziracao do JOGL
+    public static void init() {
+        GLProfile.initSingleton();
+        GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCapabilities caps = new GLCapabilities(profile);
+        window = GLWindow.create(caps);
+        window.setSize(screenWidth, screenHeight);
+        //window.setResizable(false);
+
+        //Habilita o teclado : cena
+        window.addGLEventListener(new EventListener());
+
+        //window.requestFocus();
+        FPSAnimator animator = new FPSAnimator(window, 60);
+        animator.start(); //inicia o loop de animacao
+
+        //encerrar a aplicacao adequadamente
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDestroyNotify(WindowEvent e) {
+                animator.stop();
+                System.exit(0);
+            }
+        });
+
+        //window.setFullscreen(true);        
+        window.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        init();
+    }
+}
